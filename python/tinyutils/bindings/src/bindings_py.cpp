59e2330b05ce48ae4f4c5362cc53a2c8d5ec82fc
wpumacay <wpumacay@gmail.com>
2020-05-05 02:20:09 -0500

    [Bindings]: added bindings for perlin-noise mod.

--- python/tinyutils/bindings/src/bindings_py.cpp
+++ python/tinyutils/bindings/src/bindings_py.cpp
@@ -8,6 +8,7 @@ namespace tinyutils
     extern void bindings_logging_module( py::module& m );
     extern void bindings_path_handling_module( py::module& m );
     extern void bindings_timing_module( py::module& m );
+    extern void bindings_perlin_noise_module( py::module& m );
     //// extern void bindings_profiling_module( py::module& m );
 }
 
@@ -16,4 +17,5 @@ PYBIND11_MODULE( tinyutils_py, m )
     tinyutils::bindings_logging_module( m );
     tinyutils::bindings_path_handling_module( m );
     tinyutils::bindings_timing_module( m );
+    tinyutils::bindings_perlin_noise_module( m );
 }
