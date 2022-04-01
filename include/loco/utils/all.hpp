0e8fbd8a4e159f82a1125ec1ea78b4b2bf396541
gregor <wpumacay@gmail.com>
2022-03-31 21:50:19 -0500

    [fix]: remaining rename operations

---
+++ include/loco/utils/all.hpp
@@ -0,0 +1,8 @@
+// clang-format off
+#include <loco/utils/common.hpp>
+#include <loco/utils/logging.hpp>
+#include <loco/utils/path_handling.hpp>
+#include <loco/utils/perlin_noise.hpp>
+#include <loco/utils/profiling.hpp>
+#include <loco/utils/timing.hpp>
+// clang-format on
