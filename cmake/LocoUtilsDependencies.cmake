2254196303bb91e7edcc32d3b524979688120bec
gregor <wpumacay@gmail.com>
2022-08-15 11:44:20 -0500

    [feat]: integrates find_or_fetch for handling deps

---
+++ cmake/LocoUtilsDependencies.cmake
@@ -0,0 +1,54 @@
+# ~~~
+# CMake configuration for third-party dependencies.
+#
+# Dependencies:
+# * spdlog
+# * pybind11
+#
+# - Based on the superbuild script by jeffamstutz for ospray
+#   https://github.com/jeffamstutz/superbuild_ospray/blob/main/macros.cmake
+# - Based on MuJoCo's helper find_or_fetch content macro
+#   https://github.com/deepmind/mujoco/blob/main/cmake/FindOrFetch.cmake
+# ~~~
+
+# cmake-format: off
+# ------------------------------------------------------------------------------
+# Spdlog is used for the logging functionality (internally uses the fmt lib)
+# ------------------------------------------------------------------------------
+loco_find_or_fetch_dependency(
+  USE_SYSTEM_PACKAGE FALSE
+  PACKAGE_NAME spdlog
+  LIBRARY_NAME spdlog
+  GIT_REPO https://github.com/gabime/spdlog.git
+  GIT_TAG v1.9.2
+  TARGETS spdlog::spdlog
+  BUILD_ARGS
+    -DSPDLOG_BUILD_SHARED=OFF
+    -DSPDLOG_BUILD_EXAMPLE=OFF
+    -DSPDLOG_BUILD_EXAMPLE_HO=OFF
+    -DSPDLOG_BUILD_TESTS=OFF
+    -DSPDLOG_BUILD_TESTS_HO=OFF
+    -DSPDLOG_BUILD_BENCH=OFF
+  EXCLUDE_FROM_ALL)
+
+# ------------------------------------------------------------------------------
+# Pybind11 is used for generating Python bindings for this project's C++ API.
+# Notice that we're using a forked version in which usage of unique-ptr is
+# allowed, as we use this functionality in some other parent projects
+# ------------------------------------------------------------------------------
+
+# Hint to Pybind11 that we want to use the newer FindPython module from CMake
+set(PYBIND11_FINDPYTHON ON CACHE BOOL "Use newer FindPython (CMake 3.15+)")
+
+loco_find_or_fetch_dependency(
+  USE_SYSTEM_PACKAGE FALSE
+  PACKAGE_NAME pybind11
+  LIBRARY_NAME pybind11
+  GIT_REPO https://github.com/pybind/pybind11.git
+  GIT_TAG v2.10.0
+  TARGETS pybind11::headers
+  BUILD_ARGS
+    -DPYBIND11_TEST=OFF
+  EXCLUDE_FROM_ALL)
+
+# cmake-format: on
